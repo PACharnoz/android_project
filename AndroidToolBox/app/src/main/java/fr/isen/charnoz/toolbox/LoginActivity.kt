a363b8433861ddc901652e24fee97c7c2db739a0
Paul-Adrien Charnoz <paul-adrien.charnoz@isen.yncrea.fr>
2020-01-21 08:48:00 +0100

    On rattrape le temps perdu
    on commence à retrouver ce qu'on avait hier

--- AndroidToolBox/app/src/main/java/fr/isen/charnoz/toolbox/LoginActivity.kt
+++ AndroidToolBox/app/src/main/java/fr/isen/charnoz/toolbox/LoginActivity.kt
@@ -1,12 +1,37 @@
 package fr.isen.charnoz.toolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val goodIdentifier = "paul-adrien.charnoz@ien.yncrea.fr"
+    val goodPassword = "123"
+
+    fun canLog(identifier: String, password:String): Boolean{
+        return identifier == goodIdentifier && password == goodPassword
+    }
+
+    fun doLogin(){
+        if(canLog(usernameEditText.text.toString(), inputPasswordEdit.text.toString())){
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        validateButton.setOnClickListener {
+            doLogin()
+        }
+
+
+
+
+
+
     }
 }
